867a89dea280aaec5469f83b9ff870a9452693eb
CarolineBellaLane <carolinelane@live.co.uk>
2023-11-08 13:47:54 +0000

    adds javascript file and begins to pseudocode challenge

---
+++ assets/js/index.js
@@ -0,0 +1,25 @@
+
+
+
+//Display the current day at the top of the calender when a user opens the planner.
+
+
+// Present timeblocks for standard business hours when the user scrolls down.
+//create table using div in html
+
+
+// Color-code each timeblock based on past, present, and future when the timeblock is viewed.
+//adjust css using jquery depending on click events
+
+
+// Allow a user to enter an event when they click a timeblock
+//click event to enter time block
+//form for input?
+
+
+// Save the event in local storage when the save button is clicked in that timeblock.
+//local storsge code for jquery
+
+
+// Persist events between refreshes of a page
+//recall from local storage?
