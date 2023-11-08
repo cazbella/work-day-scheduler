a8040182f6ea0f6d1a904f552f47c106c407dd70
CarolineBellaLane <carolinelane@live.co.uk>
2023-11-08 18:20:53 +0000

    adds links to jQuery and js file, adds code for currentDay and event listener for this function

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,38 @@
 
+//get elements
+
 
 
 //Display the current day at the top of the calender when a user opens the planner.
 
+var timeEl = $('#currentDay');
+
+//adds event listener to remove delay and lets DOM load first
+document.addEventListener('DOMContentLoaded', function() {
+    //timeEl stores currentDay
+    var timeEl = document.getElementById('currentDay');
+
+    function displayDay() {
+        var rightNow = dayjs().format('dddd D MMMM YYYY [at] h:mm:ss a');
+        timeEl.textContent = rightNow;
+    }
+
+    displayDay();
+    setInterval(displayDay, 1000);
+});
+
+
+// //time display code from lesson/research
+//works but there is a delay when loading
+// function displayDay() {
+//     var rightNow = dayjs().format('dddd D MMMM YYYY [at] hh:mm:ss a'); timeEl.text(rightNow);
+//     $("#currentDay").text(rightNow);
+// };
+
+
+// $(document).ready(function () {
+//     var rootElement = $('#root');
+// });
 
 // Present timeblocks for standard business hours when the user scrolls down.
 //create table using div in html
@@ -23,3 +53,4 @@
 
 // Persist events between refreshes of a page
 //recall from local storage?
+
