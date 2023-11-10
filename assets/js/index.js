7a57131ffcfe7f36dd8e8b2a8e94a3ecd45f6c79
CarolineBellaLane <carolinelane@live.co.uk>
2023-11-10 10:23:26 +0000

    adds codeto update calendar display with the current date, events retrieved from localStorage, and CSS classes to indicate whether time slots are in the past, present, or future

--- assets/js/index.js
+++ assets/js/index.js
@@ -57,9 +57,7 @@ $(document).ready(function () {
   //need conditional to check whether there are events stored for the current hour (i) within the events object.
   //there are events for the current hour, need to set the value of the timeSlot element to the corresponding event text. If there are no events, need to clear the text area by setting its value to an empty string (like in lesson code)
 //Then
-// Color-code each timeblock based on past, present, and future when the timeblock is viewed.//use CSS classes in style.css
-
-//adjust css using jquery depending on click events
+// Color-code each timeblock based on past, present, and future when the timeblock is viewed.//use CSS classes in style.css//need if statemnent 
 
 
 // Allow a user to enter an event when they click a timeblock
@@ -67,10 +65,45 @@ $(document).ready(function () {
 //form for input?
 
 
-// Save the event in local storage when the save button is clicked in that timeblock.
-//local storsge code for jquery
 
 
-// Persist events between refreshes of a page
-//recall from local storage?
 
+//function to update calendar display with the current date, events retrieved from localStorage, and CSS classes to indicate whether time slots are in the past, present, or future.
+//used internet/lesson for help here.
+
+$(document).ready(function () {
+    //need to identify/select elements
+  var container = $('.calendar-container');
+  var currentDateElement = $('#currentDate');
+  var currentDate = dayjs(); // today's date
+
+  function updateCalendar() {
+    //updates the text content of the HTML element with the id "currentDate" to display the current date in a specific format using the format method of the currentDate object
+    currentDateElement.text(currentDate.format('dddd, MMMM D, YYYY'));
+    // get data from the browser's localStorage based on the current date in format ('YYYY-MM-DD'). It parses the JSON data, and if there is no data for the current date, it initializes an empty object as the value of the events variable.
+    var events = JSON.parse(localStorage.getItem(currentDate.format('YYYY-MM-DD'))) || {};
+    //clear classes each day to esure colours are correct
+    container.find('.time-block').removeClass('past present future');
+
+    //loop over the time slots to check for past/present/future to re-assign classes for slot colours 
+    for (var i = 9; i <= 18; i++) {
+      var timeBlock = $('#' + i);
+      var timeSlot = $('#time-slot' + (i - 8));
+    //checks if there's an event stored in the events object for the current time slot (i). If there is, it sets the value of the corresponding timeSlot input element to the event. If there isn't, it sets the timeSlot input element value to an empty string.
+      if (events[i]) {
+        timeSlot.val(events[i]);
+      } else {
+        timeSlot.val('');
+      }
+      //adds CSS classes ('past', 'present', or 'future') to the timeBlock element based on a comparison between the current time slot (i) and the current hour of the day from currentDate. If the time slot is in the past, it gets the 'past' class; if it's the current hour, it gets the 'present' class; otherwise, it gets the 'future' class
+      if (i < currentDate.hour()) {
+        timeBlock.addClass('past');
+      } else if (i === currentDate.hour()) {
+        timeBlock.addClass('present');
+      } else {
+        timeBlock.addClass('future');
+      }
+    }
+  }
+
+ 
