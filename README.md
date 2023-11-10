d8e0ef47fcad2bb130f9f5594fcf0c1dd2c620d1
CarolineBellaLane <carolinelane@live.co.uk>
2023-11-10 17:17:23 +0000

    adds save event function and colour classes worked through with tutor. Begins to add delete buttons

--- README.md
+++ README.md
@@ -1,2 +1,77 @@
 # work-day-scheduler
 A calendar application that allows a user to save events for each hour of the day.
+
+https://cazbella.github.io/
+
+This project is designed to solve the challenges set out in the Module 7 assignment. It primarily uses JavaScript with some bootstrap and begins to use third party APIs.  
+
+## Description
+
+This project is an application that presents the user with quiz questions about intermediate level javascript. 
+
+Please find a screenshot below of the landing area with the console;
+
+![Screenshot of landing area](assets/images/Screenshot-quiz-landing-area-with-console.png)
+
+This project aims to show my current skills in using JavaScript that we have learned on the course so far, solving the requirements of the Module 07 Challenge in the EdX Frontend Web Development Bootcamp. 
+
+One of the hardest things was 
+
+I have completed a JavaScript course previously, but I had not encountered some of the syntax required. I have used a lot of the logic that we talked about in class this week. 
+
+## Usage
+
+Please find a link to the live application here; 
+
+https://cazbella.github.io/
+
+1. Please click the area inside the hour required in order to input text
+
+![Screenshot of first question](assets/images/Screenshot-first-question.png)
+
+2. If the 
+
+![Screenshot of first question](assets/images/Screenshot-notification.png)
+
+3. The user works 
+
+![Screenshot of score/initials input screen](assets/images/Screenshot-score-initials.png)
+
+
+
+4. When the user 
+
+![Screenshot of ](assets/images/Screenshot-scores-list.png)
+
+
+__The Console__
+_______________
+It is a requirement that there are no errors in the console.
+
+Please open the HTML file in the browser and inspect the page. Then please view the console. You should see that there are no errors. The screenshots also show this. 
+
+__A Note about Pseudocode__
+_____________________________
+
+The JavaScript files that contain this code contains lots of comments. The tutor was asked if this was good practice and the advice given was that as I am new to web development, there should be lots of comments at this stage. Hopefully the comments and the commits explain the process I followed to complete the assignment. I have also made notes here to help me remember the process and what the syntax means. I feel that I am progressing in this area and need to use fewer comments this time. 
+
+__A Note about Function__
+_________________________
+
+I have tried several things to try and
+
+
+## References
+
+I completed the Northcoders pre-work JavaScript module, so I had a foundation knowledge of some of the concepts covered. I have used google a lot this week, including Stack Overflow, W3 schools and MDN web docs. I have used ask BCS this week for help with GitHub. We also used the 1:1 tutor session this week to look at assignment 6. One page in particular that I used this week was;
+
+
+
+## Contributing
+
+I would very much appreciate some feedback on the methods I have used to achieve the task. Is this the most concise code I could have used? I would also like to know if the readme layout and content was appropriate for the task if possible please. 
+
+
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,39 +1,30 @@
 
-//get elements
-
-//Display the current day at the top of the calender when a user opens the planner.
-
-var timeEl = $('#currentDay');
-
 //adds event listener (document.ready) to remove delay and lets DOM load first. There is a delay without this.
 //code from example in lesson with modifications
-$(document).ready(function() {
-
-    var timeEl = $('#currentDay');
-
-    function displayDay() {
-        var rightNow = dayjs().format('dddd D MMMM YYYY [at] hh:mm:ss a'); timeEl.text(rightNow);
-    }
-
-    displayDay();
-    setInterval(displayDay, 1000);
-});
-//to put date to scroll
 $(document).ready(function () {
-  var currentDate = dayjs(); // dayjs() gets today's date
+
+  var timeEl = $('#currentDay');
   var container = $('.container');
   var currentDateElement = $('#currentDate');
 
-  function updateCalendar() {
-    currentDateElement.text(currentDate.format('dddd, MMMM D, YYYY'));
-
-    // need to be able to update time blocks
-    
+  function displayDay() {
+    var rightNow = dayjs().format('dddd D MMMM YYYY [at] hh:mm:ss a'); timeEl.text(rightNow);
   }
+
+  displayDay();
+  setInterval(displayDay, 1000);
+  // });
+  //to put date to scroll
+  // $(document).ready(function () {
+  var currentDate = dayjs(); // dayjs() gets today's date
+  console.log(currentDate)
+
 
   // "Previous Day" button click
   $('#prevDay').click(function () {
     currentDate = currentDate.subtract(1, 'day');
+    console.log("inthe prev click : ", currentDate)
+    console.log("inthe prev click MONTH: ", currentDate.$D)
     updateCalendar();
   });
 
@@ -43,11 +34,7 @@ $(document).ready(function () {
     updateCalendar();
   });
 
-  // Calendar call function
-  updateCalendar();
-});
-
-//Next need to
+  //Next need to
   //load items from local storage on page 
   //or use the ' ' empty bracket code to clear tect area if no even - like in the lesson
   //update 'currentDate' with date of current day of the CALENDAR - not visible. link to actual time
@@ -56,54 +43,74 @@ $(document).ready(function () {
   //need variables to store elements after finding current value of i
   //need conditional to check whether there are events stored for the current hour (i) within the events object.
   //there are events for the current hour, need to set the value of the timeSlot element to the corresponding event text. If there are no events, need to clear the text area by setting its value to an empty string (like in lesson code)
-//Then
-// Color-code each timeblock based on past, present, and future when the timeblock is viewed.//use CSS classes in style.css//need if statemnent 
+  //Then
+  // Color-code each timeblock based on past, present, and future when the timeblock is viewed.//use CSS classes in style.css//need if statemnent 
 
 
-// Allow a user to enter an event when they click a timeblock
-//click event to enter time block
-//form for input?
-
-
-
-
-
-//function to update calendar display with the current date, events retrieved from localStorage, and CSS classes to indicate whether time slots are in the past, present, or future.
-//used internet/lesson for help here.
-
-$(document).ready(function () {
-    //need to identify/select elements
   var container = $('.calendar-container');
   var currentDateElement = $('#currentDate');
-  var currentDate = dayjs(); // today's date
+  // var currentDate = dayjs(); // today's date
 
+  
   function updateCalendar() {
+    console.log("in the update fnction: ", currentDate.$D)
     //updates the text content of the HTML element with the id "currentDate" to display the current date in a specific format using the format method of the currentDate object
     currentDateElement.text(currentDate.format('dddd, MMMM D, YYYY'));
     // get data from the browser's localStorage based on the current date in format ('YYYY-MM-DD'). It parses the JSON data, and if there is no data for the current date, it initializes an empty object as the value of the events variable.
     var events = JSON.parse(localStorage.getItem(currentDate.format('YYYY-MM-DD'))) || {};
     //clear classes each day to esure colours are correct
-    container.find('.time-block').removeClass('past present future');
+    container.find('.row').removeClass('past present future');
 
     //loop over the time slots to check for past/present/future to re-assign classes for slot colours 
     for (var i = 9; i <= 18; i++) {
       var timeBlock = $('#' + i);
       var timeSlot = $('#time-slot' + (i - 8));
-    //checks if there's an event stored in the events object for the current time slot (i). If there is, it sets the value of the corresponding timeSlot input element to the event. If there isn't, it sets the timeSlot input element value to an empty string.
+
+
+      //checks if there's an event stored in the events object for the current time slot (i). If there is, it sets the value of the corresponding timeSlot input element to the event. If there isn't, it sets the timeSlot input element value to an empty string.
       if (events[i]) {
         timeSlot.val(events[i]);
       } else {
         timeSlot.val('');
       }
-      //adds CSS classes ('past', 'present', or 'future') to the timeBlock element based on a comparison between the current time slot (i) and the current hour of the day from currentDate. If the time slot is in the past, it gets the 'past' class; if it's the current hour, it gets the 'present' class; otherwise, it gets the 'future' class
-      if (i < currentDate.hour()) {
+      //adds CSS classes ('past', 'present', or 'future') to the timeBlock element based currentDate
+
+      //makes all past grey
+      if (currentDate.$D < currentDate.get("month")) {
+        console.log("past!!!!!!!")
         timeBlock.addClass('past');
-      } else if (i === currentDate.hour()) {
-        timeBlock.addClass('present');
-      } else {
+      }
+      //makes all future green
+      else if (currentDate.$D > currentDate.get("month")) {
         timeBlock.addClass('future');
+      }
+      //this block for current day
+      else {
+        if (i < currentDate.hour()) {
+          timeBlock.addClass('past');
+        } else if (i === currentDate.hour()) {
+          timeBlock.addClass('present');
+        } else {
+          timeBlock.addClass('future');
+        }
       }
     }
   }
 
- 
+
+
+  function saveEvent() {
+    var timeBlockId = $(this).parent().attr('id');
+    var eventText = $(this).siblings('.activity').val();
+    //code from class - string to object
+    //fetch from local storage
+    //if no data - empty object
+    var events = JSON.parse(localStorage.getItem(currentDate.format('YYYY-MM-DD'))) || {};
+    events[parseInt(timeBlockId)] = eventText;
+    localStorage.setItem(currentDate.format
+      //converted to string
+      ('YYYY-MM-DD'), JSON.stringify(events));
+  }
+
+
+});
